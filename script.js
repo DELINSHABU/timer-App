059e28cfdcc10c9e81b74a92f9bbde5588675eee
Delin.dev <82861020+DELINSHABU@users.noreply.github.com>
2023-11-12 04:49:22 +0530

    just upload core file of the project

---
+++ script.js
@@ -0,0 +1,22 @@
+// getting html
+function getid( id){
+ return document.getElementById(id)
+}
+// sec digit 
+var hrnum =getid("hrnum")
+var secnum =getid("secnum")
+var misecnum = getid("misecnum")
+
+// btns
+getid(startbtn,"startbtn")
+getid(stopbtn,"stopbtn")
+getid(resetbtn,"resestbtn")
+
+// fun for timer digit
+
+
+
+ function micsecnumfun(){
+    var  minode = 0
+}
+micsecnumfun()
